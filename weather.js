8db0c17bab53e5ddf8a067157d8e41fe68f2a5e2
chuanfengliu0118 <72955348+chuanfengliu0118@users.noreply.github.com>
2021-10-18 12:06:05 +0800

    Add files via upload

---
+++ weather.js
@@ -0,0 +1,22 @@
+var vg_1 = "weather.json";
+vegaEmbed("#bar_chart", vg_1).then(function(result) {
+  //Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+var vg_2 = "vis2.vg.json";
+
+vegaEmbed("#vis2", vg_2).then(function(result){
+
+}).catch(console.error);
+
+var vg_3 = "vis3.vg.json";
+
+vegaEmbed("#vis3", vg_3).then(function(result){
+
+}).catch(console.error);
+
+
+var vg_4 = "vis4.vg.json";
+
+vegaEmbed("#vis4", vg_4).then(function(result){
+
+}).catch(console.error);
